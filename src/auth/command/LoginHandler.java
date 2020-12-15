678a5d53953aaecd24e621598df4ed6fcb8a2c87
User <bom7768@gmail.com>
2020-12-15 17:42:39 +0900

    2020 12 15

--- src/auth/command/LoginHandler.java
+++ src/auth/command/LoginHandler.java
@@ -11,60 +11,60 @@ import auth.service.LoginService;
 import auth.service.User;
 import mvc.command.CommandHandler;
 
-
-public class LoginHandler implements CommandHandler{
+public class LoginHandler implements CommandHandler {
 	private static final String FORM_VIEW = "loginForm";
 	private LoginService loginService = new LoginService();
 	
-	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception{
-		if(req.getMethod().equalsIgnoreCase("GET")) {
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if (req.getMethod().equalsIgnoreCase("get")) {
 			return processForm(req, res);
-		}else if(req.getMethod().equalsIgnoreCase("POST")) {
+		} else if (req.getMethod().equalsIgnoreCase("post")) {
 			return processSubmit(req, res);
-		}else {
+		} else {
 			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 			return null;
 		}
 		
-		// TODO Auto-generated method stub
+	}
 
-	}
-	
-	private String processForm(HttpServletRequest req, HttpServletResponse res) {
-		return FORM_VIEW;
-	}
-	
-	private String processSubmit(HttpServletRequest req, HttpServletResponse res) throws Exception{
-		String id= trim(req.getParameter("id"));
-		String password= trim(req.getParameter("password"));
+	private String processSubmit(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String id = trim(req.getParameter("id"));
+		String password = trim(req.getParameter("password"));
 		
-		Map<String, Boolean> errors= new HashMap<>();
+		Map<String, Boolean> errors = new HashMap<>();
 		req.setAttribute("errors", errors);
 		
-		if(id == null || id.isEmpty())
-			errors.put("id", Boolean.TRUE);
+		if (id == null || id.isEmpty()) {
+			errors.put("id", true);
+		}
+		if (password == null || password.isEmpty()) {
+			errors.put("password", true);
+		}
 		
-		if(password == null|| password.isEmpty() )
-			errors.put("password", Boolean.TRUE);
-		
-		if(!errors.isEmpty()) {
+		if (!errors.isEmpty()) {
 			return FORM_VIEW;
 		}
 		
 		try {
-			User user= loginService.login(id, password);
+			User user = loginService.login(id, password);
 			req.getSession().setAttribute("authUser", user);
 			res.sendRedirect(req.getContextPath() + "/index.jsp");
-			
 			return null;
-			
-		}catch (LoginFailException e) {
-			errors.put("idOrPwNotMatch", Boolean.TRUE);
+		} catch (LoginFailException e) {
+			errors.put("idOrPwNotMatch", true);
 			return FORM_VIEW;
 		}
-	}
-	 private String trim(String str) {
-		 return str == null ? null :str.trim();
-	 }
+		
 	}
 
+	private String trim(String str) {
+		return str == null ? null : str.trim();
+	}
+
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
+		return FORM_VIEW;
+	}
+	
+	
+}
